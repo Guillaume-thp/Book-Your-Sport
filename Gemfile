0d113ca4fc6aaae4fe08618d4ea9c55fbb9f69f8
Guillaume Vidale <guillaume.vidale@gmail.com>
2019-12-06 14:38:16 +0100

    mailer letter opener ok once signup

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.5.1'
 
 
+gem "letter_opener", :group => :development
 gem 'watir'
 gem 'selenium-webdriver'
 gem 'faker'
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_12_04_112320) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
