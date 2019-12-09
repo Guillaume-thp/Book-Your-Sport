6e379c00c539ea822c73fc162469db1311d3f9e2
LinkPhoenix <linkblue11@outlook.com>
2019-12-09 21:22:49 +0100

    better routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
   get '/static_pages/team', to: 'static_pages#team'
   get '/static_pages/about', to: 'static_pages#about'
-  devise_for :users
-  resources :timeslots
   resources :users
-  root to: 'users#index'
+  resources :timeslots
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
