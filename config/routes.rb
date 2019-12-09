f8c6d724e3a37900042f3002d48267f9b93f5196
LinkPhoenix <linkblue11@outlook.com>
2019-12-09 22:31:52 +0100

    fix routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'users#index'
   get '/static_pages/team', to: 'static_pages#team'
   get '/static_pages/about', to: 'static_pages#about'
+  devise_for :users
   resources :users
   resources :timeslots
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
