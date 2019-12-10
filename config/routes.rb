7b89284e63875284bed39333c92afd2de01f429c
Guillaume Vidale <guillaume.vidale@gmail.com>
2019-12-10 11:13:53 +0100

    adding stripe in the app

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :timeslots
   resources :users
+  resources :charges
   root to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
