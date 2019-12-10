bd063afd4f2727fe00059d480e2f9edcd524f511
Guillaume Vidale <guillaume.vidale@gmail.com>
2019-12-10 13:26:39 +0100

    merge with master ok

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,8 @@ Rails.application.routes.draw do
   get '/static_pages/about', to: 'static_pages#about'
   devise_for :users
   resources :users
-<<<<<<< HEAD
   resources :charges
   root to: 'users#index'
-=======
   resources :timeslots
->>>>>>> 20aee43a015573e6db0bf168469eebe9b9aa5c5c
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
