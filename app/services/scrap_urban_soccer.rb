fc336769b01fbfb0df11cf40f380c5eac2a99527
hilal-w <hilal-w@live.com>
2019-12-11 16:15:43 +0100

    duration params done

--- app/services/scrap_urban_soccer.rb
+++ app/services/scrap_urban_soccer.rb
@@ -1,40 +1,37 @@
 
 class ScrapUrbanSoccer
-    def initialize
-    end
+ class << self
+    def perform(city,time,duration)
+    
+          opts = {
+        headless: true
+      }
   
+      if (chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil))
+        opts.merge!( options: {binary: chrome_bin})
+      end 
   
-    def perform(city,time)
-  
-        opts = {
-      headless: true
-    }
-  
-    if (chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil))
-      opts.merge!( options: {binary: chrome_bin})
-    end 
-  
-  browser = Watir::Browser.new :chrome, opts
-  browser.goto 'https://my.urbansoccer.fr/user?goto=reserver'
-  email_field = browser.text_field(type: 'email')
-  password_field = browser.text_field(type: 'password')
-  email_field.set(ENV['URBAN_EMAIL'])
-  password_field.set(ENV['URBAN_PASSWORD'])
+      browser = Watir::Browser.new :chrome  #, opts
+      browser.goto 'https://my.urbansoccer.fr/user?goto=reserver'
+      email_field = browser.text_field(type: 'email')
+      password_field = browser.text_field(type: 'password')
+      email_field.set(ENV['URBAN_EMAIL'])
+      password_field.set(ENV['URBAN_PASSWORD'])
  
   
-  password_field.send_keys(:enter)
+      password_field.send_keys(:enter)
  
   
-  idf = browser.path(id: "IDF")
-  sleep rand(0.1..0.3)
-  idf.double_click
+      idf = browser.path(id: "IDF")
+      sleep rand(0.1..0.3)
+      idf.double_click
   
-  puteaux = browser.div(text: city)
+      puteaux = browser.div(text: city)
   
-  sleep rand(0.1..0.3)
-  puteaux.wait_until(&:present?).double_click
-  # cliquer sur la fleche droite du calendrier pour le mois suivant
-   #browser.span(class:["DayPicker-NavButton", "DayPicker-NavButton--next"]).click
+      sleep rand(0.1..0.3)
+      puteaux.wait_until(&:present?).double_click
+      # cliquer sur la fleche droite du calendrier pour le mois suivant
+      #browser.span(class:["DayPicker-NavButton", "DayPicker-NavButton--next"]).click
   
   
   
@@ -42,24 +39,43 @@ class ScrapUrbanSoccer
   
  
   
-  browser.option(value:time).click
-  binding.irb
-  
+      browser.option(value:time).click
+      
   #dropdown durée
-  browser.option(value: "120").click
-  
-  date = browser.div(text: "21") # Choisir le jour. Faire en dernier sinon bug sur chrome
-  date.click
-  
-  
-  
-  #Type de terrain. Remplacer un tiret par un underscore dans un attribut.
-  browser.span(text:"3 contre 3").click
-  
-  
-  return browser.div(class: "reservation-header").text #browser.div(class: "reservation-header").text
-    end
+  if duration == "01:00"
+    duration = "60"
+  elsif duration =="01:30"
+    duration ="90"
+  elsif duration == "02:00"
+    duration = "120"
   end
+
+      browser.option(value:duration).click
+     
+      browser.span(text:"Intérieur").click
+      binding.irb
+      date = browser.div(text: "21") # Choisir le jour. Faire en dernier sinon bug sur chrome
+      date.click
+  
+  
+  
+  
+  
+  
+      return browser.div(class: "reservation-header").text #browser.div(class: "reservation-header").text
+    end
+ 
+
+ private
+   
+   
+    def urban_duration(duration)
+     
+        
+        return duration
+    end
+end  
+end
   
   
   #type de terrain : browser.element(:xpath => "//div[2]/span[2]").text
@@ -67,5 +83,4 @@ class ScrapUrbanSoccer
   # durée : browser.element(:xpath => "//div[2]/div[2]/span[2]").text
   # browser.element(:xpath => "//div[2]/div[3]/span[2]).text
   # prix : browser.element(:xpath => "//div[2]/div[3]/span[2]").text
-  
   
