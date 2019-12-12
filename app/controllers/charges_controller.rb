7ca0dfb038cf36c71cc19516114b7d9573454cb6
Guillaume Vidale <guillaume.vidale@gmail.com>
2019-12-12 18:45:29 +0100

    stripe ok with confirmation message

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class ChargesController < ApplicationController
-  def new; end
+  def new 
+    
+  end
 
   def create
     # Amount in cents
+    @date = params[:date]
+    @hour = params[:hour]
+    @duration = params[:duration]
     @amount = params[:amount]
+
+    if booking.save
+      redirect_to 
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
