3e037115fc7669995a4641077f33fcbff117369a
Guillaume Vidale <guillaume.vidale@gmail.com>
2019-12-13 12:11:24 +0100

    front without about page/ route deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#welcome'
   get '/static_pages/team', to: 'static_pages#team'
-  get '/static_pages/about', to: 'static_pages#about'
   devise_for :users
   resources :users
   resources :charges
