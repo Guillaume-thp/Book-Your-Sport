1bf50495c9053a955a7312819f14681031218f97
LinkPhoenix <linkblue11@outlook.com>
2019-12-13 17:50:49 +0100

    TEAM: review code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,39 +15,35 @@
 //= require activestorage
 //= require turbolinks
 
-
-
+// Select elements in the form search
 function getSelectValue() {
-    var sportType = document.getElementById("sport_element").value;
-    console.log(sportType);
-    var date = document.getElementById("date_element").value;
-    console.log(date);
-    var time = document.getElementById("time_element").value;
-    console.log(time);
-    var city = document.getElementById("city_element").value;
-    console.log(city);
-
+  var sportType = document.getElementById("sport_element").value;
+  console.log(sportType);
+  var date = document.getElementById("date_element").value;
+  console.log(date);
+  var time = document.getElementById("time_element").value;
+  console.log(time);
+  var city = document.getElementById("city_element").value;
+  console.log(city);
 }
 
+// Take value of search in var
 function process() {
-    var sportType = document.getElementById("sport_element").value;
-
-    var date = document.getElementById("date_element").value;
-    var time = document.getElementById("time_element").value;
-
-    var city = document.getElementById("city_element").value;
-
-    display_values(city, date, time, sportType);
-
+  var sportType = document.getElementById("sport_element").value;
+  var date = document.getElementById("date_element").value;
+  var time = document.getElementById("time_element").value;
+  var city = document.getElementById("city_element").value;
+  display_values(city, date, time, sportType);
 }
 
+// Print informations of form search
 function display_values(city, date, time, sportType) {
-    document.getElementById("city").innerHTML = city
-    console.log(document.getElementById("city"))
-    document.getElementById("sportType").innerHTML = sportType
-    console.log(document.getElementById("sportType"))
-    document.getElementById("date").innerHTML = date
-    console.log(document.getElementById("date"))
-    document.getElementById("time").innerHTML = time
-    console.log(document.getElementById("time"))
+  document.getElementById("city").innerHTML = city;
+  console.log(document.getElementById("city"));
+  document.getElementById("sportType").innerHTML = sportType;
+  console.log(document.getElementById("sportType"));
+  document.getElementById("date").innerHTML = date;
+  console.log(document.getElementById("date"));
+  document.getElementById("time").innerHTML = time;
+  console.log(document.getElementById("time"));
 }
--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 class ChargesController < ApplicationController
-  def new 
-    
+  def new
+    # NEW
   end
 
   def create
     # Amount in cents
-
-    
-end
+  end
 end
