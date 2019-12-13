2a12c9a1b3d6faefe52db6c035a29bddd56f05a0
LinkPhoenix <linkblue11@outlook.com>
2019-12-13 18:31:43 +0100

    TEAM: remove unless route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :charges
-  root to: 'users#index'
   resources :timeslots
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
